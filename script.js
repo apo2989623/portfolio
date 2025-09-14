73b1f0805f9aedb329190acbdf18d99eabb53699
apo2989623 <kirill87190@gmail.com>
2025-09-14 10:22:03 +0300

    0/09

---
+++ script.js
@@ -0,0 +1,9 @@
+const buttons = document.querySelectorAll(".menu_link");
+const active = document.querySelector(".active");
+
+
+function apo(){
+    console.log("Hello");
+}
+window.addEventListener("scroll", apo);
+
