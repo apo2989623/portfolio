6e9b527537798c064a4b1e1f43cbf869f3034291
apo2989623 <kirill87190@gmail.com>
2025-09-14 21:55:11 +0300

    0.9.3

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const buttons = document.querySelectorAll(".menu_link");
 const active = document.querySelector(".active");
+const sections = document.querySelectorAll("section");
 
 
 function apo(){
-    console.log("Hello");
-}
+    const position = window.scrollY; 
+    console.log(position)
+   
+
+ 
+    let targetSection = null;
+
+  
+   for (let section of sections){
+        const offsetBottom = section.offsetTop + section.offsetHeight;
+        if (section.offsetTop < position && position < offsetBottom) {
+           targetSection = section;
+           break;
+        }
+    }
+    console.log(targetSection);
+} 
 window.addEventListener("scroll", apo);
 
